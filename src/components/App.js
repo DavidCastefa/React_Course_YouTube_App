312e800c97057ff6a3cad92087a55723894702a3
David <DRhoton@vinyacastelldefels.com>
2021-08-24 13:11:38 +0200

    Default search changed

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import useVideos from '../hooks/useVideos';
 
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
-  const [videos, search] = useVideos('D222');
+  const [videos, search] = useVideos('D222 Discipleship');
 
 useEffect(() => {
   setSelectedVideo(videos[0]);
